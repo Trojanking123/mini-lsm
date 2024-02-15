4bb234c1dca7b77cfae22160927ba8219d765320
trojanking123 <wuqianking123@gmail.com>
2024-02-15 23:02:01 +0800

    week 1 day2 task2

--- mini-lsm-starter/src/iterators/merge_iterator.rs
+++ mini-lsm-starter/src/iterators/merge_iterator.rs
@@ -2,6 +2,7 @@
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
 use std::cmp::{self};
+use std::collections::binary_heap::PeekMut;
 use std::collections::BinaryHeap;
 
 use anyhow::Result;
@@ -10,6 +11,7 @@ use crate::key::KeySlice;
 
 use super::StorageIterator;
 
+#[derive(Clone)]
 struct HeapWrapper<I: StorageIterator>(pub usize, pub Box<I>);
 
 impl<I: StorageIterator> PartialEq for HeapWrapper<I> {
@@ -42,12 +44,28 @@ impl<I: StorageIterator> Ord for HeapWrapper<I> {
 /// iterators, perfer the one with smaller index.
 pub struct MergeIterator<I: StorageIterator> {
     iters: BinaryHeap<HeapWrapper<I>>,
-    current: HeapWrapper<I>,
+    current: Option<HeapWrapper<I>>,
 }
 
 impl<I: StorageIterator> MergeIterator<I> {
     pub fn create(iters: Vec<Box<I>>) -> Self {
-        unimplemented!()
+        let mut i = 0;
+        let mut iters = iters;
+        let total_len = iters.len();
+        let mut heap = BinaryHeap::new();
+        while let Some(item) = iters.pop() {
+            if item.is_valid() {
+                let hw = HeapWrapper(total_len - i - 1, item);
+                heap.push(hw);
+                i += 1;
+            }
+        }
+        let cur = heap.pop();
+        let merged_iter = MergeIterator {
+            iters: heap,
+            current: cur,
+        };
+        merged_iter
     }
 }
 
@@ -57,18 +75,43 @@ impl<I: 'static + for<'a> StorageIterator<KeyType<'a> = KeySlice<'a>>> StorageIt
     type KeyType<'a> = KeySlice<'a>;
 
     fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.current.as_ref().unwrap().1.key()
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        self.current.as_ref().unwrap().1.value()
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        !self.current.as_ref().is_none()
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        let mut cur = std::mem::replace(&mut self.current, None).unwrap();
+        while let Some(mut it) = self.iters.peek_mut() {
+            if it.1.key() == cur.1.key() {
+                match it.1.next() {
+                    Err(e) => return Err(e),
+                    Ok(_) => {}
+                };
+                if !it.1.is_valid() {
+                    PeekMut::pop(it);
+                }
+            } else if it.1.key() > cur.1.key() {
+                break;
+            }
+        }
+
+        match cur.1.next() {
+            Err(e) => return Err(e),
+            Ok(_) => {}
+        };
+        if cur.1.is_valid() {
+            self.iters.push(cur);
+        }
+
+        let cur = self.iters.pop();
+        let _ = std::mem::replace(&mut self.current, cur);
+        Ok(())
     }
 }
